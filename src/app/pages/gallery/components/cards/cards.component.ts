270c3220ccdfe9f824463a2918c0f96cb1c52be7
samuelaboahasare@gmail.com <samuel.aboah@amalitech.com>
2024-05-31 17:52:19 +0200

    Feat: auth and added images

--- src/app/pages/gallery/components/cards/cards.component.ts
+++ src/app/pages/gallery/components/cards/cards.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, SimpleChanges, inject, input } from '@angular/core';
+import { imageArray } from './images';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +9,47 @@ import { Component } from '@angular/core';
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss',
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  private http = inject(HttpClient);
+  searchQuery = input<string>();
   cards = [1, 2, 3, 4, 5, 5, 45, 45, 454, 45, 2, 3, 4, 5, 5, 45, 45, 454, 45];
-  // isTallCard(index: number) {
-  //   return (index + 1) % 5 === 0;
-  // }
+
+  imageArray: {
+    id: number;
+    src: string;
+    filename: string;
+  }[] = [];
+
+  ngOnInit(): void {
+    this.imageArray = imageArray;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['searchQuery']) {
+      this.filterData();
+    }
+  }
+
+  filterData() {
+    if (this.searchQuery()) {
+      this.imageArray = imageArray.filter((img) =>
+        img.filename.includes(this.searchQuery() as string)
+      );
+
+      return;
+    }
+
+    this.imageArray = imageArray;
+  }
+
+  downloadImage(img: string, filename: string): void {
+    this.http.get(img, { responseType: 'blob' }).subscribe((blob) => {
+      const link = document.createElement('a');
+      const url = URL.createObjectURL(blob);
+      link.href = url;
+      link.download = filename;
+      link.click();
+      URL.revokeObjectURL(url); // Clean up the URL object
+    });
+  }
 }
