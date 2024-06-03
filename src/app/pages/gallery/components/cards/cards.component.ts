65799369de42e747bfd2e96eb681e6de58dbe11c
samuelaboahasare@gmail.com <samuel.aboah@amalitech.com>
2024-06-03 12:33:48 +0200

    cards images and loading more

--- src/app/pages/gallery/components/cards/cards.component.ts
+++ src/app/pages/gallery/components/cards/cards.component.ts
@@ -1,18 +1,29 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, SimpleChanges, inject, input } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  HostListener,
+  OnInit,
+  SimpleChanges,
+  inject,
+  input,
+} from '@angular/core';
 import { imageArray } from './images';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CardsComponent implements OnInit {
   private http = inject(HttpClient);
   searchQuery = input<string>();
-  cards = [1, 2, 3, 4, 5, 5, 45, 45, 454, 45, 2, 3, 4, 5, 5, 45, 45, 454, 45];
+  isPreview: boolean = false;
+  currImgPreview: string = '';
 
   imageArray: {
     id: number;
@@ -21,7 +32,9 @@ export class CardsComponent implements OnInit {
   }[] = [];
 
   ngOnInit(): void {
-    this.imageArray = imageArray;
+    // this.imageArray = imageArray;
+
+    this.loadImages();
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -42,14 +55,59 @@ export class CardsComponent implements OnInit {
     this.imageArray = imageArray;
   }
 
-  downloadImage(img: string, filename: string): void {
-    this.http.get(img, { responseType: 'blob' }).subscribe((blob) => {
+  handlePreview(img: string) {
+    this.isPreview = true;
+    this.currImgPreview = img;
+  }
+
+  downloadImage(img: { src: string; filename: string }, event: Event) {
+    event.stopPropagation();
+    this.http.get(img.src, { responseType: 'blob' }).subscribe((blob) => {
       const link = document.createElement('a');
       const url = URL.createObjectURL(blob);
       link.href = url;
-      link.download = filename;
+      link.download = img.filename;
       link.click();
-      URL.revokeObjectURL(url); // Clean up the URL object
+      URL.revokeObjectURL(url);
     });
+  }
+
+  isLoading: boolean = false;
+  private page: number = 1;
+  private pageSize: number = 20; // Number of items to load per page
+  public hasMore: boolean = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      this.loadMore();
+    }
+  }
+
+  loadImages(): void {
+    // Initial load
+    this.imageArray = imageArray.slice(0, this.pageSize * this.page);
+  }
+
+  loadMore(): void {
+    if (this.isLoading) {
+      return;
+    }
+    this.isLoading = true;
+
+    // Simulate an API call
+    setTimeout(() => {
+      const moreImages = imageArray.slice(
+        this.pageSize * (this.page - 1),
+        this.pageSize * this.page
+      );
+      if (moreImages.length === 0) {
+        this.hasMore = false; // No more items to load
+      } else {
+        this.page++;
+        this.imageArray = [...this.imageArray, ...moreImages];
+      }
+      this.isLoading = false;
+    }, 2000); // Delay to simulate network latency
   }
 }
